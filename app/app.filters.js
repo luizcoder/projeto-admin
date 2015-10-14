5232d4821c0a0d3f3a03fd97574f0dca69834ae3
José Luiz Martins Oliveira <martinsmanager@gmail.com>
2015-10-14 17:21:51 -0300

    Adiciona filters globais

---
+++ app/app.filters.js
@@ -0,0 +1,14 @@
+(function(){
+	var app = angular.module('app');
+	app.filter('range', function() {
+	  return function(input, total) {
+	    total = parseInt(total);
+
+	    for (var i=1; i<=total; i++) {
+	      input.push(i);
+	    }
+
+	    return input;
+	  };
+	});
+})();
