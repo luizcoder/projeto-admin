dfbb62ff381a0c66d3c739dfd343e55f90b87f6d
José Luiz Martins Oliveira <martinsmanager@gmail.com>
2015-10-14 17:46:42 -0300

    Altera lista de usuários
    Adicionada paginação e funções de navegação e pesquisa
    nos dados dos usuários retornados pela api

--- app/components/user/userController.js
+++ app/components/user/userController.js
@@ -1,19 +1,55 @@
 (function(){
     var app = angular.module('user', []);
     app.controller("UserController", ["$scope","$http",'$rootScope', function($scope,$http,$rootScope){
-        var apiUrl = $rootScope.apiUrl;
-        
-        var ctrl = this;        
-        ctrl.getUsers = function(){
-            
-            $http.get(apiUrl+'/api/user').success(function(data){
+
+        $scope.getUsers = function(){
+
+            var data = {page: 1, per_page: 10,search: null};
+            if($scope.search){
+                data.search = $scope.search;
+            }
+            if($scope.table){
+                data.page = $scope.table.current_page;
+                data.per_page = $scope.per_page;
+            }           
+            $http.get($rootScope.apiUrl+'/api/user', {params: data}).success(function(data){
                 
-                ctrl.users = data;    
+                $scope.table = data;    
                 
             }).error(function(error){
                 
-                ctrl.error = error;
+                $scope.error = error;
             });  
+
+        }
+
+        $scope.proxima = function(){
+
+            if($scope.table.current_page + 1 <= $scope.table.last_page){
+                $scope.table.current_page += 1;
+                $scope.getUsers();
+            }
+
+        }
+        $scope.anterior = function(){
+
+            if($scope.table.current_page - 1 > 1 ){
+                $scope.table.current_page -= 1;
+                $scope.getUsers();
+            }
+
+        }        
+        $scope.pagina = function(page){
+
+            $scope.table.current_page = page;
+            $scope.getUsers();
+
+        }  
+
+        $scope.doSearch = function(){
+
+            $scope.getUsers();
+            
         }
         
     }]);
