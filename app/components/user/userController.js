77bc5663fc20a2e91626da4cc3caa42ac397c1ed
José Luiz Martins Oliveria <martinsmanager@gmail.com>
2015-10-19 10:55:39 -0200

    Adicionar flag readOnly e remover duplicidade
    Adicionada flag para controle de edição e visualização
    do registro. Removido código duplicado e ajustado algumas
    funções.

--- app/components/user/userController.js
+++ app/components/user/userController.js
@@ -4,7 +4,6 @@
     app.controller("UserController", ["$scope","$http",'$rootScope','$uibModal', function($scope,$http,$rootScope, $uibModal){
 
         $scope.getUsers = function(){
-
             var data = {page: 1, per_page: 10,search: null};
             if($scope.search){
                 data.search = $scope.search;
@@ -12,15 +11,15 @@
             if($scope.table){
                 data.page = $scope.table.current_page;
                 data.per_page = $scope.per_page;
-            }           
+            }
             $http.get($rootScope.apiUrl+'/api/user', {params: data}).success(function(data){
-                
-                $scope.table = data;    
-                
+
+                $scope.table = data;
+
             }).error(function(error){
-                
+
                 $scope.error = error;
-            });  
+            });
         }
 
 
@@ -28,45 +27,32 @@
          * Controles de paginação
          */
         $scope.proxima = function(){
-
             if($scope.table.current_page + 1 <= $scope.table.last_page){
                 $scope.table.current_page += 1;
                 $scope.getUsers();
             }
-
         }
 
         $scope.anterior = function(){
-
             if($scope.table.current_page - 1 > 1 ){
                 $scope.table.current_page -= 1;
                 $scope.getUsers();
             }
-
-        }   
-
-        $scope.pagina = function(page){
-
-            $scope.table.current_page = page;
-            $scope.getUsers();
-
-        }  
-
-        $scope.doSearch = function(){
-
-            $scope.getUsers();
-            
         }
 
+        $scope.pagina = function(page){
+            $scope.table.current_page = page;
+            $scope.getUsers();
+        }
+
+        $scope.doSearch = function(){
+            $scope.getUsers();
+        }
 
         /**
          * Ações do registro
          */
         $scope.acoes = function (registro) {
-
-
-            $scope.usuario = registro;
-
             var modalInstance = $uibModal.open({
                 animation: true,
                 templateUrl: 'app/components/user/userAction.html',
@@ -75,59 +61,23 @@
                 size: 'sm',
                 resolve: {
                     registro: function(){
-                        return $scope.usuario;
+                        return registro;
                         },
                 }
             });
         };
-        
-    }]);
 
+    }]);
 
     app.controller('UserActionModalController', ['$modalInstance','$scope','registro','$uibModal', function($modalInstance,$scope,registro,$uibModal){
 
-
         $scope.visualizar = function(){
+            registro.readOnly = true;
             $scope.mostrarForm(registro);
         }
 
         $scope.alterar = function(){
-            $scope.mostrarForm(registro);
-        }
-
-
-        $scope.fechar = function(){
-            $modalInstance.dismiss('cancel');
-        }
-
-        $scope.mostrarForm = function (registro) {
-            $scope.usuario = registro;
-
-            var modalInstance = $uibModal.open({
-                animation: true,
-                templateUrl: 'app/components/user/userAction.html',
-                controller:"UserActionModalController",
-                windowTopClass:"modal-action",
-                size: 'sm',
-                resolve: {
-                    registro: function(){
-                        return $scope.usuario;
-                        },
-                }
-            });
-        };
-        
-    }]);
-
-
-    app.controller('UserActionModalController', ['$modalInstance','$scope','registro','$uibModal', function($modalInstance,$scope,registro,$uibModal){
-
-
-        $scope.visualizar = function(){
-            $scope.mostrarForm(registro);
-        }
-
-        $scope.alterar = function(){
+            registro.readOnly = false;
             $scope.mostrarForm(registro);
         }
 
@@ -136,34 +86,31 @@
         }
 
         $scope.mostrarForm = function (registro) {
-            $scope.usuario = registro;
             var modalInstance = $uibModal.open({
                 animation: true,
                 templateUrl: 'app/components/user/userForm.html',
                 controller:"UserModalController",
                 resolve: {
-                registro: function () {
-                    return $scope.usuario;
-                    }
+                    registro: function () {
+                        return registro;
+                    },
                 }
             });
         };
 
-    }]);  
+    }]);
 
 
     app.controller('UserModalController', ['$modalInstance','$scope', 'registro','$rootScope','$http', function($modalInstance,$scope,registro,$rootScope,$http){
 
-        //Copiando o registro para edição
-        $scope.usuario = angular.copy(registro);
-
+        // Copiando o registro para edição
+        $scope.registro = angular.copy(registro);
 
         $scope.fechar = function(){
             $modalInstance.dismiss('cancel');
         }
 
         $scope.cancelar = function(){
-            $scope.usuario = $scope.usuario_old;
             $modalInstance.dismiss('cancel');
         }
 
@@ -171,26 +118,23 @@
          * Salvando alterações no registro
          */
         $scope.salvar = function(){
-
-            var data = $scope.usuario;
+            var data = $scope.registro;
             $http.put($rootScope.apiUrl+'/api/user/' + data.id, data).success(function(data){
-                
-                //retornando o registro já alterado
-                $scope.usuario = data;
 
-                //Atualizando o registro na listagem 
-                for(var key in $scope.usuario) {
-                    registro[key] = $scope.usuario[key];
+                // Retornando o registro já alterado
+                $scope.registro = data;
+
+                // Atualizando o registro na listagem
+                for(var key in $scope.registro) {
+                    registro[key] = $scope.registro[key];
                 }
-
- 
 
             }).error(function(error){
                 $scope.error = error;
-            });  
+            });
 
         }
 
     }]);
-    
+
 })();
