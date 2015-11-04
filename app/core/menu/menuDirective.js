3f507840818468d1dea3e6450dfcd70e36f73e53
José Luiz Martins Oliveria <martinsmanager@gmail.com>
2015-11-04 14:12:04 -0200

    Adicionar diretiva para links ativos

--- app/core/menu/menuDirective.js
+++ app/core/menu/menuDirective.js
@@ -55,4 +55,39 @@
         }
     });
 
+    /*
+     * Directiva para adicionar class 'active' em link ativos
+     */
+    app.directive('isActive', function($state){
+        return {
+            restrict: 'A',
+            link: function (scope, element,attr) {
+
+                // Função avalida se o link esta ativo
+                var setClass = function(){
+
+                    // Caso o atributo possua um valor
+                    if(attr.isActive.length > 0 && $state.current.name.indexOf(attr.isActive)>=0){
+                        angular.element(element).parent('li').addClass('active');
+
+                    // Senão, é feito a leitura do uiSref
+                    }else if( attr.uiSref.length > 0 && attr.uiSref == $state.current.name){
+                        angular.element(element).parent('li').addClass('active');
+
+                    // Caso não seja um link ativo, remove a classe 'active'
+                    }else{
+                        angular.element(element).parent('li').removeClass('active');
+                    }
+
+                }
+                scope.$on("$locationChangeStart", function (event, next, current) {
+                    setClass();
+                });
+
+                //Executa ao inicializar o elemento
+                setClass();
+          },
+        }
+    });
+
 })();
